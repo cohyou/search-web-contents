9b09e6324dd30b825069c60ceca0a2ab01465a0a
OKAZAKI Yasuo <cohyou@ironoir.io>
2016-05-18 22:42:00 +0900

    add files

---
+++ README.md
@@ -0,0 +1,5 @@
+# search-web-contents package
+
+search by google with selected text.
+
+<!---[A screenshot of your package](https://f.cloud.github.com/assets/69169/2290250/c35d867a-a017-11e3-86be-cd7c5bf3ff9b.gif)-->
---
+++ spec/search-web-contents-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SearchWebContentsView from '../lib/search-web-contents-view';
+
+describe('SearchWebContentsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
